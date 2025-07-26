7587387270f20a97322133911d5e9e01b618a639
Courtney Macgregor <cjoss90@hotmail.com>
2025-07-26 12:53:39 +1000

    feat(quote-form): fetch portPairs and containers on mount

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function QuoteForm() {
+  const [portPairs, setPortPairs] = useState([]);
+  const [containers, setContainers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${ProcessingInstruction.env.REACT_APP_API_URL}/containers`)
+      .then((res) => res.json())
+      .then(setContainers);
+  }, []);
+
   return <h1>Quote Form</h1>;
 }
 
