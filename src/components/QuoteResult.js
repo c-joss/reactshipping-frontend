dd624caab8aad3cbcef723aaf822dcbaf3d6532b
Courtney Macgregor <cjoss90@hotmail.com>
2025-07-28 10:19:01 +1000

    feat(QuoteResult): add Book Now link passing quote and container data to booking form

--- src/components/QuoteResult.js
+++ src/components/QuoteResult.js
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 function QuoteResult() {
   const { state } = useLocation();
@@ -11,41 +11,49 @@ function QuoteResult() {
   const rate = quote?.rates.find((r) => r.containerId === Number(containerId));
 
   return (
-    <div>
-      <h1>Quote Result</h1>
-      {quote && rate ? (
-        <table border="1" cellPadding="10">
-          <thead>
-            <tr>
-              <th>Origin</th>
-              <th>Destination</th>
-              <th>Container</th>
-              <th>Freight</th>
-              <th>THC</th>
-              <th>DOC</th>
-              <th>DHC</th>
-              <th>LSS</th>
-              <th>Transit Time (days)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{origin}</td>
-              <td>{destination}</td>
-              <td>{containerType}</td>
-              <td>{rate.freight}</td>
-              <td>{rate.thc}</td>
-              <td>{rate.doc}</td>
-              <td>{rate.dhc}</td>
-              <td>{rate.lss}</td>
-              <td>{quote.transitTime}</td>
-            </tr>
-          </tbody>
-        </table>
-      ) : (
-        <p>No quote found.</p>
-      )}
-    </div>
+    <>
+      <div>
+        <h1>Quote Result</h1>
+        {quote && rate ? (
+          <table border="1" cellPadding="10">
+            <thead>
+              <tr>
+                <th>Origin</th>
+                <th>Destination</th>
+                <th>Container</th>
+                <th>Freight</th>
+                <th>THC</th>
+                <th>DOC</th>
+                <th>DHC</th>
+                <th>LSS</th>
+                <th>Transit Time (days)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{origin}</td>
+                <td>{destination}</td>
+                <td>{containerType}</td>
+                <td>{rate.freight}</td>
+                <td>{rate.thc}</td>
+                <td>{rate.doc}</td>
+                <td>{rate.dhc}</td>
+                <td>{rate.lss}</td>
+                <td>{quote.transitTime}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p>No quote found.</p>
+        )}
+      </div>
+      <Link
+        to="/booking"
+        state={{ origin, destination, containerId, containerType, quote }}
+      >
+        Book Now
+      </Link>
+    </>
   );
 }
 
